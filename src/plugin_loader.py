585392bd2845965e1caf4791a47573a20e64ca79
B07 <102260018+bohmiiidd@users.noreply.github.com>
2025-08-20 16:13:05 +0100

    Add comment for plugin class name

--- src/plugin_loader.py
+++ src/plugin_loader.py
@@ -7,6 +7,6 @@ def load_plugin():
     for file in os.listdir(plugin_dir):
         if file.endswith(".py") and not file.startswith("__"):
             name = file[:-3]
-            module = __import__(f"src.plugins.{name}", fromlist=["Plugin"])
-            plugins[name] = module.Plugin()
+            module = __import__(f"src.plugins.{name}", fromlist=["Plugin"]) 
+            plugins[name] = module.Plugin() # plugins class name should be 'Plugin'
     return plugins
